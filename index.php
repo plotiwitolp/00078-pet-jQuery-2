1c939bf56b635ff0c4b586a897238e7790348b27
plotiwitolp <plotiwitolp@gmail.com>
2022-09-13 14:56:24 +0400

    start

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>00078-pet-jQuery-2</title>
+  <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+  <header class="header">
+    <div class="container">
+      <div class="header__logo">
+        <img src="./img/logo-main.png">
+      </div>
+      <nav class="header-menu">
+        <ul>
+          <li class="header-menu__item"><a href="#">test 1</a></li>
+          <li class="header-menu__item"><a href="#">test 2</a></li>
+          <li class="header-menu__item"><a href="#">test 3</a></li>
+          <li class="header-menu__item"><a href="#">test 4</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <main class="main">
+    <div class="container">
+      <div class="slider-1">
+        <div class="viewport">
+          <div class="slider-1__item">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere obcaecati asperiores, quam cupiditate ducimus dolorum maiores! Quae nobis distinctio, pariatur praesentium animi cupiditate vero sint minus quia autem velit qui.</div>
+          <div class="slider-1__item">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere obcaecati asperiores, quam cupiditate ducimus dolorum maiores! Quae nobis distinctio, pariatur praesentium animi cupiditate vero sint minus quia autem velit qui.</div>
+          <div class="slider-1__item">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere obcaecati asperiores, quam cupiditate ducimus dolorum maiores! Quae nobis distinctio, pariatur praesentium animi cupiditate vero sint minus quia autem velit qui.</div>
+        </div>
+        <div class="controller">
+
+        </div>
+      </div>
+    </div>
+  </main>
+  <footer class="footer">
+    <div class="container">
+
+    </div>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+  <script src="./script.js"></script>
+</body>
+
+</html>
